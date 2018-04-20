0322bedadab8ca0fa04de1c0fe55495895e3841e
Graeme Brough <gdbrough@blueyonder.co.uk>
2018-04-20 11:26:57 +0100

    initial commit

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,24 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('can add', function(){
+    calculator.clearClick();
+    calculator.numberClick(2);
+    calculator.numberClick(0);
+    calculator.operatorClick("+");
+    calculator.numberClick(5);
+    calculator.operatorClick("=");
+    assert.equal(25, calculator.runningTotal);
+  })
+
+});
